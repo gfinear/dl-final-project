6c9ed264e19751c17bc6a903ff39b7c65621156a
exing1 <edward_xing@brown.edu>
2022-12-06 17:04:33 -0500

    style key

--- code/summary_generation/summary_model.py
+++ code/summary_generation/summary_model.py
@@ -31,7 +31,7 @@ class SummaryModel():
             caption_data = pickle.load(cf)
             summary_data = pickle.load(sf)
 
-        caption_style = caption_data['style']
-        summary_style = summary_data['style']
+        caption_style = caption_data['train_style']
+        summary_style = summary_data['train_style']
 
         return caption_style, summary_style
