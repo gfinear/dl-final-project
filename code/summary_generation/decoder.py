77d9b8e222fc3405559f98a8519a462a20b22c12
exing1 <edward_xing@brown.edu>
2022-12-08 15:01:54 -0500

    running summary

--- code/summary_generation/decoder.py
+++ code/summary_generation/decoder.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 
 
 class Decoder(tf.keras.Model):
-    def __init__(self, vocab_size=None, hidden_size=128, **kwargs):
+    def __init__(self, vocab_size=None, hidden_size=1028, **kwargs):
         super().__init__(**kwargs)
         self.vocab_size = vocab_size
         self.hidden_size = hidden_size
@@ -25,6 +25,9 @@ class Decoder(tf.keras.Model):
             self.perplexity_tracker = tf.keras.metrics.Mean(name='perplexity')
 
     def call(self, inputs):
+        if self.vocab_size is None:
+            return self.decoder(inputs)
+
         summaries, st_vectors = inputs
         st_embeddings = self.skipthought_embedding(st_vectors)
         summary_embeddings = self.embedding(summaries)
@@ -56,21 +59,3 @@ class Decoder(tf.keras.Model):
     def perplexity(self, truth, predicted):
         scce = tf.keras.losses.SparseCategoricalCrossentropy()
         return tf.exp(scce(truth, predicted))
-
-    def generate_summary(self, summary_skipthough, wordToIds, temp, window_length):
-        idsToWords = {id: word for word, id in wordToIds.items()}
-        unk_token = wordToIds['<unk>']
-        padId = wordsToIds['<pad>']
-        summary_so_far = [wordToIds['<start>']]
-        while len(summary_so_far) < window_length and summary_so_far[-1] != wordToIds['<end>']:
-            summary_input = np.array([summary_so_far + ((window_length - len(summary_so_far)) * [padID])])
-            logits = self(np.expand_dims(summary_skipthough, 0), summary_input)
-            logits = logits[0][len(summary_so_far) - 1]
-            probs = tf.nn.softmax(logits / temp).numpy()
-            next_token = unk_token
-            attempts = 0
-            while next_token == unk_token and attempts < 5:
-                next_token = np.random.choice(len(probs), p=probs)
-                attempts += 1
-            summary_so_far.append(next_token)
-        return ' '.join([idsToWords[x] for x in summary_so_far][1:-1])
--- code/summary_generation/summary_model.py
+++ code/summary_generation/summary_model.py
@@ -1,28 +1,50 @@
+import numpy as np
 import pickle
+import tensorflow as tf
 
 from .decoder import Decoder
 from .encoder import Encoder
 
 class SummaryModel():
 
-    def __init__(self, temperature=0.05):
+    def __init__(self, temperature=0.05, length=251):
+        self.temperature = temperature
+        self.length = length
+
         self.encoder = Encoder()
         self.decoder = Decoder()
 
         self.caption_style, self.summary_style = self.load_styles()
-        self.wordToIds = self.load_wordToIds()
-        self.temperature = temperature
+        self.wordToId = self.load_summary_data()
 
     def __call__(self, caption):
         if isinstance(caption, str):
             caption = [caption]
         caption_skipthought = self.encoder(caption)
         summary_skipthought = self.transfer_style(caption_skipthought)
-        summary = self.decoder.generate_summary(summary_skipthought, self.wordToIds, self.temperature, 250)
+        summary = self.generate_summary(summary_skipthought)
         return summary
 
     def transfer_style(self, embedding):
         return embedding - self.caption_style + self.summary_style
+
+    def generate_summary(self, skipthought):
+        idsToWords = {id: word for word, id in self.wordToId.items()}
+        unk_token = self.wordToId['<unk>']
+        padId = self.wordToId['<pad>']
+        summary = [self.wordToId['<start>']]
+        while len(summary) < self.length and summary[-1] != self.wordToId['<end>']:
+            summary_input = np.array([summary + ((self.length - len(summary)) * [padId])])
+            logits = self.decoder((summary_input, skipthought))
+            logits = logits[0][len(summary) - 1]
+            probs = tf.nn.softmax(logits / self.temperature).numpy()
+            next_token = unk_token
+            attempts = 0
+            while next_token == unk_token and attempts < 5:
+                next_token = np.random.choice(len(probs), p=probs)
+                attempts += 1
+            summary.append(next_token)
+        return ' '.join([idsToWords[x] for x in summary][1:-1])
 
     def load_styles(self):
         embedding_path = 'summary_generation/skipthought_embeddings/'
@@ -38,7 +60,7 @@ class SummaryModel():
 
         return caption_style, summary_style
 
-    def load_wordToIds(self):
+    def load_summary_data(self):
         summary_data_path = '../data/summary_data.p'
         with open(summary_data_path, 'rb') as f:
             summary_data = pickle.load(f)
